b907b3bd6242173dd00da595ce676a7414595f6b
chapcs <benchapman003@gmail.com>
2023-04-23 11:06:18 -0400

    Added VendingMachine baseclass and AnimalFeedVendingMachine subclass

--- Program.cs
+++ Program.cs
@@ -6,3 +6,32 @@
         Console.WriteLine(vendingMachine.Dispense(2.00M));
     }
 }
+
+class VendingMachine
+{
+    public virtual string Item { get; }
+
+    protected virtual bool CheckAmount(decimal money)
+    {
+        return false;
+    }
+
+    public string Dispense(decimal money)
+    {
+        if (CheckAmount(money)) return Item;
+        else return "Please enter the right amount";
+    }
+}
+
+class AnimalFeedVendingMachine : VendingMachine
+{
+    public override string Item 
+    { 
+        get { return "a handful of animal feed"; } 
+    }
+
+    protected override bool CheckAmount(decimal money)
+    {
+        return money >= 1.25M;
+    }
+}
